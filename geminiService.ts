cd8472a4c20eac8b0b716e4f337de77dbe630fef
[dyad] <Tulovy2007@gmail.com>
2026-01-01 19:09:07 +0700

    Updated geminiService.ts

--- geminiService.ts
+++ geminiService.ts
@@ -1,7 +1,7 @@
 import { UserInput, SuggestionResponse, SuggestionMeal } from "./types";
 
 // --- QUAN TRỌNG: DÁN KEY MỚI VÀO DÒNG DƯỚI ---
-const API_KEY = "DÁN_KEY_MỚI_CỦA_BẠN_VÀO_ĐÂY"; 
+const API_KEY = "AIzaSyDf3VXB6lOd39RwRe0_ggr3ckBaqCXvUnU"; 
 const BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models";
 
 // --- CẤU HÌNH ---
